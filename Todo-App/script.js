5f26787f0e9442b38f29f83882bf52e9c1e1f806
shreyas kulkarni <shrecool1412@gmail.com>
2025-09-14 17:25:33 +0530

    Updated Todo-App code in index.html,style.css,script.js

--- Todo-App/script.js
+++ Todo-App/script.js
@@ -1,3 +1,22 @@
+const addbutton = document.getElementById("Addbtn");
+const input   = document.getElementById("input");
+const tasklist = document.getElementById("tasklist");
 
 
-console.log("Todo-App is Running ");
+addbutton.addEventListener("click",function(){
+   const li = document.createElement("li");
+  li.textContent = input.value.trim();
+
+  tasklist.appendChild(li);
+
+
+   
+
+  li.addEventListener("click",function(){
+    li.remove();
+  })
+
+
+  input.value = '';
+   
+})
