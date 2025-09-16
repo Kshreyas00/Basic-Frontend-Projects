fb0a28c4d8fddef9b6e0a547841b2b2183e1d68c
shreyas kulkarni <shrecool1412@gmail.com>
2025-09-16 16:52:35 +0530

    Update Todo App with functionality of delete and done buttons

--- Todo-App/script.js
+++ Todo-App/script.js
@@ -4,16 +4,43 @@ const tasklist = document.getElementById("tasklist");
 
 
 addbutton.addEventListener("click",function(){
-   const li = document.createElement("li");
+  const li = document.createElement("li");
   li.textContent = input.value.trim();
 
+  const donebtn = document.createElement("button");
+  const deletebtn = document.createElement("button");
+
+  donebtn.textContent = "Done";
+  deletebtn.textContent = "Delete";
+
+  donebtn.id = "donebutton";
+  deletebtn.id = "deletedutton";
+
+   li.appendChild(donebtn);
+   li.appendChild(deletebtn);
+
+
   tasklist.appendChild(li);
+
 
 
    
 
   li.addEventListener("click",function(){
-    li.remove();
+    
+      donebtn.addEventListener("click",function(){
+      li.style.textDecoration = 'line-through';
+      li.style.color = "yellowgreen";
+
+      deletebtn.addEventListener("click",function(){
+        li.style.backgroundColor = "red";
+        alert("If you want to delete this task");
+        li.remove();
+      })
+
+    
+  })
+
   })
 
 
