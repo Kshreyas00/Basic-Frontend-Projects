10f2b0e88a7772f206deef907ce8ab9ffe521064
shreyas kulkarni <shrecool1412@gmail.com>
2025-09-27 21:51:47 +0530

    Update Todo  App With Todos status Bar

--- Todo-App/script.js
+++ Todo-App/script.js
@@ -3,6 +3,27 @@ const input    = document.getElementById("input");
 const taskList  = document.getElementById("task-list");
 
 
+
+
+function UpdateTaskCount()
+{
+  const allTasks = taskList.querySelectorAll("li");
+  let completeCount = 0;
+  let pendingCount = 0;
+
+  allTasks.forEach(task=>{
+    if(task.iscompleted){
+      completeCount++;
+    }
+    else{
+      pendingCount++;
+    }
+  })
+
+   console.log(`Completed: ${completeCount}, Pending: ${pendingCount}`);
+}
+
+
 Addbutton.addEventListener("click",function(){
     if(input.value===''){
         alert("Please Enter Your Task");
@@ -11,6 +32,8 @@ Addbutton.addEventListener("click",function(){
     const li = document.createElement("li");
     const donebutton = document.createElement("button");
     const deletebutton = document.createElement("button");
+
+    li.iscompleted = false;
 
     const btnwrapperdiv = document.createElement('div');
     btnwrapperdiv.style.display = "flex";
@@ -31,11 +54,23 @@ Addbutton.addEventListener("click",function(){
     li.appendChild(btnwrapperdiv);
 
 
-    donebutton.addEventListener("click",function(){
-      li.style.textDecoration = "line-through";
+   donebutton.addEventListener("click",function(){
+    li.style.textDecoration = "line-through";
+    li.style.opacity = "0.4";
+    donebutton.textContent = "Completed";
+    donebutton.disabled = true;
+
+    li.iscompleted = true;
+
+    UpdateTaskCount();
+   })
+
+   deletebutton.addEventListener("click",function(){
+      alert("Are you confirm to delete this todo.");
+      li.remove();
+
+      UpdateTaskCount();
     })
-
-
 
     taskList.appendChild(li);
 
